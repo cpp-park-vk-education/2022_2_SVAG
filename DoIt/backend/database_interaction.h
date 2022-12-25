96af86ed00919191bbd49698cdebcbf68635dd73
Georgiy <kolomytkin2020@gmail.com>
2022-12-25 15:15:02 +0000

    Fixed database_connector and database_interaction; fixed port

--- DoIt/backend/database_interaction.h
+++ DoIt/backend/database_interaction.h
@@ -1,13 +1,18 @@
 #pragma once
 #include "database_connector.h"
 #include "BoardDataBase.h"
+#include "CardDataBase.h"
+#include "UserDataBase.h"
+
+#include "json.hpp"
+using json = nlohmann::json;
 
 struct json_to_string_response_convert
 {
     std::string operator()(json j)
     {
         std::string response = j.dump();
-        response = response + '\n';
+        response = response + "\n";
         return response;
     }
 };
@@ -15,7 +20,7 @@ struct json_to_string_response_convert
 class DatabaseInteraction
 {
 public:
-    DatabaseInteraction(): cl(getBoardClient()) {}
+    DatabaseInteraction(): cl(getBoardClient()), clUser(getUserClient()), clCard(getCardClient()) {}
     std::string analyze_msg(json msg_json);
     std::string on_login(json msg_json);
 private:
@@ -27,4 +32,6 @@ private:
 private:
     json_to_string_response_convert convert;
     BoardDataBase cl;
+    CardDataBase clCard;
+    UserDataBase clUser;
 };
